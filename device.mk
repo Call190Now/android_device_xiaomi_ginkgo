a0f7bc24f1d8e4991333103baded00c95e7c3230
PIPIPIG233666 <pig.priv@gmail.com>
2020-09-25 01:50:03 -0300

    ginkgo: Build vendor.qti.hardware.btconfigstore@1.0 from source
    Change-Id: I1f1352c1c8f76b241bb288f12a40eda34c039b7c

--- device.mk
+++ device.mk
@@ -134,9 +134,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    libbtconfigstore \
     libbthost_if \
     libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
